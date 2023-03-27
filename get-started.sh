7c72f6230290bc2020167ebf97350c0c9d870961
Phil Leggetter <phil@leggetter.co.uk>
2023-03-27 18:32:51 +0100

    chore: get-started to use published container

--- get-started.sh
+++ get-started.sh
@@ -6,5 +6,5 @@ set +a
 echo "Executing ... "
 docker run --rm --env-file .env \
     -v "$(pwd)":/workspace/php \
-    -w /workspace/php ghcr.io/mongodb-developer/get-started-php:0.1 \
+    -w /workspace/php ghcr.io/tigrisdata-community/tigris-mongodb-php-example:latest \
     "php getstarted.php"
